b12993f6000b3f389d8121d68636c8b45b6a1ade
Arpita Lonakadi <104141167+Arpl14@users.noreply.github.com>
2024-09-13 20:22:11 -0400

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,13 +2,16 @@ import os
 import streamlit as st
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from chromadb.config import Settings
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+
+
+
 
 # Load environment variables from .env file
 load_dotenv()
